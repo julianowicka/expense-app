a5c69d2ca7f28ba4af649abb3dcbcedf744f3d2b
julia nowicka <julia.nowicka@lemondev.pl>
2023-10-01 22:52:59 +0200

    Add param decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from 'src/data'
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(
+    @Param('type') type: string
+    console.log(type)
+  ) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+      return data.report.filter((report => report.type === reportType);
   }
   @Get(':id')
   getReportById() {
